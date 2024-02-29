766e40939224e1ae0ecdeefc7f51a72654ecb928
Devam kathane <dkathane42@gmail.com>
2024-02-29 12:46:10 +0530

    Data Ingesttion Complete

---
+++ src/textSummarizer/components/data_ingestion.py
@@ -0,0 +1,43 @@
+import os
+import urllib.request as request
+import zipfile
+from textSummarizer.logging import logger
+from textSummarizer.utils.common import get_size
+from pathlib import Path
+from textSummarizer.entity import DataIngestionConfig
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with following info: \n{headers}")
+        else:
+            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")
+
+    def extract_zip_file(self):
+        """
+        Extracts the zip file into the data directory
+
+        Raises:
+            ValueError: If the downloaded file is not a valid ZIP archive.
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+
+        # Check file extension before attempting extraction
+        if not self.config.local_data_file.endswith(".zip"):
+            raise ValueError("File is not a ZIP archive")
+
+        try:
+            with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+                zip_ref.extractall(unzip_path)
+        except zipfile.BadZipFile:
+            # Handle the case where the file is not a valid ZIP (e.g., log an error)
+            logger.error("Downloaded file is not a valid ZIP archive. Please check the source URL.")
--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -1,11 +1,13 @@
 import os
-from box.exception import BoxValueError
+from box.exceptions import BoxValueError
 import yaml
-from textSummarizer import logger
+from textSummarizer.logging import logger
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+
+
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
@@ -31,28 +33,32 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     except Exception as e:
         raise e
     
+
+
 @ensure_annotations
-def create_directories(path_to_directories: list, verbose= True):
-    """Create list of directories
-    
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
     Args:
         path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False
-        """
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"created directories at: {path}")
+            logger.info(f"created directory at: {path}")
+
+
 
 @ensure_annotations
-def get_size(path:path) ->str:
+def get_size(path: Path) -> str:
     """get size in KB
-    
+
     Args:
-        path(path): path to the file
-        
+        path (Path): path of the file
+
     Returns:
-        str: size in kb
+        str: size in KB
     """
-    size_in_kb= round(os.path.getsize(path)/1024)
-    return f"~(size_in_kb) KB"
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
